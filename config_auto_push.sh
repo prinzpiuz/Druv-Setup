2050c04011efdb61be26c0aa8145c2b9412ab164
druv <druv@druv.druv.prinzpiuz.in>
2024-08-23 15:38:38 +0530

    fix(config_auto_push):bracj updated

--- config_auto_push.sh
+++ config_auto_push.sh
@@ -3,4 +3,4 @@
 #this is a program to automaticaly push system wiki
 #depends on inotify
 cd /configs || exit 
-/usr/bin/inotifywait -q -m -e CLOSE_WRITE --format="git commit -m 'auto commit:$(date)' %w && git push origin master" /configs | bash
+/usr/bin/inotifywait -q -m -e CLOSE_WRITE --format="git commit -m 'auto commit:$(date)' %w && git push origin main" /configs | bash
